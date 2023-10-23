e427b91139f712a3ab48b5de82a152677ae71148
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2023-10-23 22:31:18 +0300

    Update

--- Assets/Scripts/Calculator/Calculator.cs
+++ Assets/Scripts/Calculator/Calculator.cs
@@ -6,10 +6,10 @@ using UnityEngine.UIElements;
 
 public class Calculator
 {
-   private char _lastSymbolOpertaion;
-   private readonly string[] _buttonsAction = { "+", "-", "/", "*", "%", "C" };
-   
-   private readonly List<CalculatorButton> _buttons = new List<CalculatorButton>()
+    private char _lastSymbolOpertaion;
+    private readonly string[] _buttonsAction = { "+", "-", "/", "*", "%", "C" };
+
+    private readonly List<CalculatorButton> _buttons = new List<CalculatorButton>()
    {
       new CalculatorButtonResult("C"),
       new CalculatorButtonNumber(","),
@@ -30,100 +30,100 @@ public class Calculator
       new CalculatorButtonNumber("0"),
       new CalculatorButtonResult("="),
    };
-   
-   public void Initialization(UI ui, VisualTreeAsset buttonAsset)
-   {
-      
-      int j = 1;
-      for (int i = 0; i < _buttons.Count; i++)
-      {
-         
-         var calcButton = _buttons[i];
-         var button = buttonAsset.Instantiate().Q<Button>("button");
-         
-         button.AddToClassList("button");
-         button.AddToClassList(_buttonsAction.Contains(calcButton.Name) ? "button--action" : "button--number");
-         if(calcButton.Name == "=")
-            button.AddToClassList("button--result");
-         
-         button.text = calcButton.Name;
-         button.clicked += ()=> ButtonOnClicked(calcButton,ui.Output);
-         
-         ui.RowOfButtons.contentContainer.Add(button);
-           
-         if (j == 4)
-         {
-            ui.ContainerOfButtons.contentContainer.Add(ui.RowOfButtons);
-            ui.RowOfButtons = new VisualElement();
-            ui.RowOfButtons.AddToClassList("buttons--row");
-            j = 0;
-         }
 
-         j++;
-         
-      }
-      ui.ContainerOfButtons.contentContainer.Add(ui.RowOfButtons);
-   }
+    public void Initialization(UI ui, VisualTreeAsset buttonAsset)
+    {
 
-   private void ButtonOnClicked(CalculatorButton calcButton, Label label)
-   {
-      
-      if (calcButton is IResultButton resultButton)
-      {
-         
-         if (calcButton.Name != "=")
-            _lastSymbolOpertaion = resultButton.GetSymbolOperation();
-               
-         var textValue = label.text.Split(_lastSymbolOpertaion);
-         float numTwo = 0;
-         float numOne = 0;
+        int j = 1;
+        for (int i = 0; i < _buttons.Count; i++)
+        {
 
-         if (float.TryParse(textValue[0], out float parsedNumOne))
-            numOne = parsedNumOne;
-         if (textValue.Length > 1 && float.TryParse(textValue[1], out float parsedNumTwo))
-            numTwo = parsedNumTwo;
+            var calcButton = _buttons[i];
+            var button = buttonAsset.Instantiate().Q<Button>("button");
 
-         IOperation operation = GetOperation(_lastSymbolOpertaion);
-         string result = resultButton.Result(numOne, numTwo, operation);
-         resultButton.Print(label, result);
-               
-         return;
-         
-      }
+            button.AddToClassList("button");
+            button.AddToClassList(_buttonsAction.Contains(calcButton.Name) ? "button--action" : "button--number");
+            if (calcButton.Name == "=")
+                button.AddToClassList("button--result");
 
-      if (calcButton is IActionButton actionButton)
-         _lastSymbolOpertaion = actionButton.GetSymbolOperation();
+            button.text = calcButton.Name;
+            button.clicked += () => ButtonOnClicked(calcButton, ui.Output);
 
-      if ((label.text == string.Empty || label.text.StartsWith('0')) && calcButton.Name == "0")
-         return;
+            ui.RowOfButtons.contentContainer.Add(button);
 
-      calcButton.Print(label, label.text + calcButton.Name);
-      
-   }
+            if (j == 4)
+            {
+                ui.ContainerOfButtons.contentContainer.Add(ui.RowOfButtons);
+                ui.RowOfButtons = new VisualElement();
+                ui.RowOfButtons.AddToClassList("buttons--row");
+                j = 0;
+            }
 
-   private IOperation GetOperation(char lastOp)
-   {
-      
-      switch (lastOp)
-      {
-         
-         case '*':
-            return new MultiOperation();
-         case '-':
-            return new SubOperation();
-         case '+':
-            return new AddOperation();
-         case '/':
-            return new DivisionOperation();
-         case '%':
-            return new PersentOperation();
-         case 'C':
-            return  new ClearOperation();
-         default:
-            throw new Exception("operation not found");
-            
-      }
-      
-   }
+            j++;
+
+        }
+        ui.ContainerOfButtons.contentContainer.Add(ui.RowOfButtons);
+    }
+
+    private void ButtonOnClicked(CalculatorButton calcButton, Label label)
+    {
+        if ((label.text == string.Empty || label.text.StartsWith(calcButton.Name)) && calcButton.Name == "0"
+               || label.text.Contains(calcButton.Name) && calcButton.Name == ",")
+            return;
+
+        if (calcButton is IResultButton resultButton)
+        {
+
+            if (calcButton.Name != "=")
+                _lastSymbolOpertaion = resultButton.GetSymbolOperation();
+
+            var textValue = label.text.Split(_lastSymbolOpertaion);
+            float numTwo = 0;
+            float numOne = 0;
+
+            if (float.TryParse(textValue[0], out float parsedNumOne))
+                numOne = parsedNumOne;
+            if (textValue.Length > 1 && float.TryParse(textValue[1], out float parsedNumTwo))
+                numTwo = parsedNumTwo;
+
+            IOperation operation = GetOperation(_lastSymbolOpertaion);
+            string result = resultButton.Result(numOne, numTwo, operation);
+            resultButton.Print(label, result);
+
+            return;
+
+        }
+
+        if (calcButton is IActionButton actionButton)
+            _lastSymbolOpertaion = actionButton.GetSymbolOperation();
+
+        calcButton.Print(label, label.text + calcButton.Name);
+
+    }
+
+    private IOperation GetOperation(char lastOp)
+    {
+
+        switch (lastOp)
+        {
+
+            case '*':
+                return new MultiOperation();
+            case '-':
+                return new SubOperation();
+            case '+':
+                return new AddOperation();
+            case '/':
+                return new DivisionOperation();
+            case '%':
+                return new PersentOperation();
+            case 'C':
+                return new ClearOperation();
+            default:
+                throw new Exception("operation not found");
+
+        }
+
+    }
 
 }
